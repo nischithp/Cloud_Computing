3a53871e24f904b046d6f9e1c125957ab1248e02
Nischith <nischithp@gmail.com>
2020-10-25 20:30:14 -0500

    Added SQL file containing User, Videos data for testing Accounts API

--- cloud-computing.sql
+++ cloud-computing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 26, 2020 at 01:40 AM
+-- Generation Time: Oct 26, 2020 at 02:29 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `liked_videos` (
-  `user_id` varchar(500) NOT NULL,
-  `video_id` varchar(500) NOT NULL
+  `user_id` int(200) NOT NULL,
+  `video_id` int(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -39,18 +39,21 @@ CREATE TABLE `liked_videos` (
 --
 
 CREATE TABLE `user` (
-  `user_id` varchar(200) NOT NULL,
+  `user_id` int(200) NOT NULL,
   `username` char(100) NOT NULL,
+  `firstname` text NOT NULL,
+  `lastname` text DEFAULT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(500) NOT NULL
+  `password` varchar(500) NOT NULL,
+  `register_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `username`, `email`, `password`) VALUES
-('nish123ooaspawoe', 'nish', 'nischithp@gmail.com', 'pass123');
+INSERT INTO `user` (`user_id`, `username`, `firstname`, `lastname`, `email`, `password`, `register_time`) VALUES
+(0, 'nish', '', NULL, 'nischithp@gmail.com', 'pass123', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -59,18 +62,20 @@ INSERT INTO `user` (`user_id`, `username`, `email`, `password`) VALUES
 --
 
 CREATE TABLE `videos` (
-  `video_id` varchar(200) NOT NULL,
-  `uploaded_by` varchar(200) NOT NULL,
+  `video_id` int(200) NOT NULL,
+  `video_title` text NOT NULL,
+  `uploaded_by` int(200) NOT NULL,
   `upload_date` datetime NOT NULL,
-  `views` int(11) NOT NULL
+  `views` int(11) NOT NULL,
+  `tags` varchar(500) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `videos`
 --
 
-INSERT INTO `videos` (`video_id`, `uploaded_by`, `upload_date`, `views`) VALUES
-('akdfj0wefwriu234wefsdfasdfvsdfad', 'nish123ooaspawoe', '2020-10-25 19:38:16', 0);
+INSERT INTO `videos` (`video_id`, `video_title`, `uploaded_by`, `upload_date`, `views`, `tags`) VALUES
+(0, '', 0, '2020-10-25 19:38:16', 0, NULL);
 
 --
 -- Indexes for dumped tables
@@ -80,8 +85,8 @@ INSERT INTO `videos` (`video_id`, `uploaded_by`, `upload_date`, `views`) VALUES
 -- Indexes for table `liked_videos`
 --
 ALTER TABLE `liked_videos`
-  ADD KEY `video_id` (`video_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `video_id` (`video_id`);
 
 --
 -- Indexes for table `user`
@@ -104,14 +109,14 @@ ALTER TABLE `videos`
 -- Constraints for table `liked_videos`
 --
 ALTER TABLE `liked_videos`
-  ADD CONSTRAINT `liked_videos_ibfk_1` FOREIGN KEY (`video_id`) REFERENCES `videos` (`video_id`),
-  ADD CONSTRAINT `liked_videos_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+  ADD CONSTRAINT `liked_videos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `liked_videos_ibfk_2` FOREIGN KEY (`video_id`) REFERENCES `videos` (`video_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `videos`
 --
 ALTER TABLE `videos`
-  ADD CONSTRAINT `videos_ibfk_1` FOREIGN KEY (`uploaded_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD CONSTRAINT `videos_ibfk_1` FOREIGN KEY (`uploaded_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
