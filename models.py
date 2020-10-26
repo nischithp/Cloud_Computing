19ee7fe0fc98964a5c01d4ea07ba57245e7a89f6
Nischith <nischithp@gmail.com>
2020-10-26 16:56:34 -0500

    Added landing page and a few CSS elements to make it look a little bit nicer

--- models.py
+++ models.py
@@ -10,11 +10,7 @@ class RegForm(Form):
                  [validators.DataRequired()])
   email = StringField('Email Address', [validators.DataRequired(), 
              validators.Email(), validators.Length(min=6, max=35)])
-  password = PasswordField('New Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', 
-                           message='Passwords must match')
-        ])
+  password = PasswordField('New Password', [validators.DataRequired(),validators.EqualTo('confirm', message='Passwords must match')])
   confirm = PasswordField('Repeat Password')
   submit = SubmitField('Submit')
 
