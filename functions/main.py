cadc1a4e6293d0cc1d72fcbba8509332f5f60ff1
naveensn <naveensn100@gmail.com>
2020-10-27 14:29:46 -0500

    still updating function

--- functions/main.py
+++ functions/main.py
@@ -1,44 +1,38 @@
 import sqlalchemy
 from flask import escape, jsonify, request, make_response
-import sqlalchemy as sql
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 
 
 def user_access(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    """
 
-    # connection_name = "cloudcomputinglab-291822:us-central1:cloud-computing"
     connection_name = "cloudcomputinglab-291822:us-central1:cloud-computing"
 
-    table_name = ""
-    table_field = ""
-    table_field_value = ""
-    db_name = "video_sharing"
-    db_user = "root"
-    db_password = "admin"
-
-    driver_name = 'mysql+pymysql'
+    mysql = {"database": "video_sharing",
+             "host": "35.232.179.75",
+             "port": "127.0.0.1:3306",
+             "connection": "cloudcomputinglab-291822:us-central1:cloud-computing",
+             "user_name": "cloud-computing",
+             "password": "cloud-computing",
+             "driver": "mysql+pymysql",
+             "url": "mysql+pymysql://cloud-computing:cloud-computing@127.0.0.1:3306/video_sharing"
+             }
     query_string = dict({"unix_socket": "/cloudsql/{}".format(connection_name)})
 
-    db = sql.create_engine(sql.engine.url.URL(
-        drivername=driver_name,
-        username=db_user,
-        password=db_password,
-        database=db_name,
-        query=query_string,
-      ),
-      pool_size=5,
-      max_overflow=2,
-      pool_timeout=30,
-      pool_recycle=1800
-    )
+    engine = create_engine(mysql["url"])
+    db = scoped_session(sessionmaker(bind=engine))
+
+    class user:
+        def __index__(self, user_id, username, email, firstname, lastname, password, register_time):
+            self.user_id = user_id
+            self.username = username
+            self.email = email
+            self.firstname = firstname
+            self.lastname = lastname
+            self.password = password
+            self.register_time = register_time
+
 
     # print("request.method: ", request.method)
     # print("request.method: ", request.method)
@@ -53,12 +47,42 @@ def user_access(request):
     else:
         name = 'World'
 
-    stmt = sqlalchemy.text('select * from user')
-    try:
-        with db.connect() as conn:
-            conn.execute(stmt)
-    except Exception as e:
-        return 'Error: {}'.format(str(e))
-    return 'ok'
+    tables = db.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES")
+    return jsonify([{key: value for key, value in row.items()} for row in tables if row is not None]), 200
 
-    return jsonify({'greeting': 'Hello {}!'.format(escape(name + method))})
+    stmt = sqlalchemy.text('select * from user')
+
+    user = {"username": "Naveen",
+            "email": "naveensn100@gmial.com",
+            "firstname": "Naveen",
+            "lastname": "S N",
+            "password": "asodasnasidndai"
+           }
+
+    db.execute("DROP TABLE user")
+
+    db.execute("CREATE TABLE users ("
+               "id SERIAL PRIMARY KEY,"
+               "username VARCHAR NOT NULL,"
+               "email VARCHAR NOT NULL,"
+               "firstname VARCHAR NOT NULL,"
+               "lastname VARCHAR,"
+               "password VARCHAR NOT NULL,"
+               "date_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP)")
+
+    db.execute("INSERT INTO users (username, email, firstname, lastname, password) "
+               "VALUES (:username, :email, :firstname, :lastname, password)",
+              user)
+    db.commit()
+
+
+
+    users = db.execute(stmt).fetchall()
+    if users is not None:
+        return jsonify([{key: value for key, value in row.items()} for row in users if row is not None]), 200
+    else:
+        return jsonify([{}])
+
+
+
+    #return jsonify({'greeting': 'Hello {}!'.format(escape(name + method))})
--- functions/test.py
+++ functions/test.py
@@ -1,4 +1,5 @@
 import requests
 dictToSend = {"name": "Naveen"}
+#res = requests.post('https://us-central1-cloudcomputinglab-291822.cloudfunctions.net/user_access', json=dictToSend)
 res = requests.post('http://127.0.0.1:8080/', json=dictToSend)
 print(res.text)
