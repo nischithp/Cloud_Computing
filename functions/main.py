5e08b8b09516e3dd09f8fe51f7271de74ebde23c
naveensn <naveensn100@gmail.com>
2020-11-05 17:37:24 -0600

    made return values as JSON

--- functions/main.py
+++ functions/main.py
@@ -38,45 +38,44 @@ def user_access(request):
 
     db = scoped_session(sessionmaker(bind=eng))
 
-    error = {"bad request": {"code": 400, "message": "No JSON, request should include JSON object"},
-             "unauthorised": {"code": 401, "message": "Incorrect password, please enter correct password"},
-             "forbidden": {"code": 403, "message": "Duplicate email, email id already exists"},
-             "not found": {"code": 404, "message": "User Not found, User does not exists in data base"},
-             "bad method": {"code": 405, "message": "Method not allowed, please use post method"},
-             "unacceptable": {"code": 406, "message": "Request is not acceptable, "
+    error = {"bad request": {"status": "fail", "code": 400, "error": "No JSON, request should include JSON object"},
+             "unauthorised": {"status": "fail", "code": 401, "error": "Incorrect password, please enter correct password"},
+             "forbidden": {"status": "fail", "code": 403, "error": "Duplicate email, email id already exists"},
+             "not found": {"status": "fail", "code": 404, "error": "User Not found, User does not exists in data base"},
+             "bad method": {"status": "fail", "code": 405, "error": "Method not allowed, please use post method"},
+             "unacceptable": {"status": "fail", "code": 406, "error": "Request is not acceptable, "
                                                       "accepted requests are login, register and update"},
-             "empty field": {"code": 406, "message": "Request is not acceptable, user information can not be empty"},
-             "unprocessable": {"code": 422, "message": "JSON format is not correct"},
-             "internal": {"code": 500, "message": "Unexpected error in our database/server"}}
+             "empty field": {"status": "fail", "code": 406, "error": "Request is not acceptable, user information can not be empty"},
+             "unprocessable": {"status": "fail", "code": 422, "error": "JSON format is not correct"},
+             "internal": {"status": "fail", "code": 500, "error": "Unexpected error in our database/server"}}
 
     if request.method != "POST":
-        return error["bad method"]["message"], error["bad method"]["code"]
+        return jsonify(error["bad method"]), error["bad method"]["code"]
 
     request_json = request.get_json(silent=True)
     if isinstance(request_json, str):
         request_json = json.loads(request_json)
-    elif type(json.loads(request_json)) != dict:
-        return error["bad request"]["message"], error["bad request"]["code"]
+    elif type(request_json) != dict:
+        return jsonify(error["bad request"]), error["bad request"]["code"]
     if not request_json:
-        return error["bad request"]["message"], error["bad request"]["code"]
+        return jsonify(error["bad request"]), error["bad request"]["code"]
 
     necessary_info = ["request", "data"]
     if not all(info in request_json for info in necessary_info):
-        return error["unprocessable"]["message"], error["unprocessable"]["code"]
+        return jsonify(error["unprocessable"]), error["unprocessable"]["code"]
 
     check_user_query = sqlalchemy.text("SELECT 1 FROM users WHERE email = :email")
     get_user_query = sqlalchemy.text("SELECT id, username, firstname, lastname, email, date_time"
                                      " FROM users WHERE email = :email AND password = :password")
 
     if request_json["request"].lower() == "login":
-        print("login")
 
         necessary_info = ["email", "password"]
         if not all(info in request_json["data"] for info in necessary_info):
-            return error["unprocessable"]["message"], error["unprocessable"]["code"]
+            return jsonify(error["unprocessable"]), error["unprocessable"]["code"]
 
         if not all(request_json["data"][info] for info in necessary_info):
-            return error["empty field"]["message"], error["empty field"]["code"]
+            return jsonify(error["empty field"]), error["empty field"]["code"]
 
         try:
             if db.execute(check_user_query, {"email": request_json["data"]["email"]}).fetchall():
@@ -84,35 +83,34 @@ def user_access(request):
                 if user:
                     user = {"id": user[0], "username": user[1], "firstname": user[2],
                             "lastname": user[3], "email": user[4], "date_time": user[5]}
-                    return jsonify(user)
+                    return jsonify({"status": "pass", "user": user})
                 else:
-                    return error["unauthorised"]["message"], error["unauthorised"]["code"]
+                    return jsonify(error["unauthorised"]), error["unauthorised"]["code"]
             else:
-                return error["not found"]["message"], error["not found"]["code"]
+                return jsonify(error["not found"]), error["not found"]["code"]
 
         except SQLAlchemyError as e:
             error = str(e.__dict__['orig'])
-            return error, 500
+            return jsonify({"status": "fail", "error": error}), 500
 
     elif request_json["request"].lower() == "register":
 
-        print("register")
         necessary_info = ["username", "firstname", "email", "password"]
         if not all(info in request_json["data"] for info in necessary_info):
-            return error["unprocessable"]["message"], error["unprocessable"]["code"]
+            return jsonify(error["unprocessable"]), error["unprocessable"]["code"]
 
         if not all(request_json["data"][info] for info in necessary_info):
-            return error["empty field"]["message"], error["empty field"]["code"]
+            return jsonify(error["empty field"]), error["empty field"]["code"]
 
         try:
             user_in_db = db.execute(check_user_query, {"email": request_json["data"]["email"]}).fetchall()
 
         except SQLAlchemyError as e:
             error = str(e.__dict__['orig'])
-            return error, 500
+            return jsonify({"status": "fail", "error": error}), 500
 
         if user_in_db:
-            return error["forbidden"]["message"], error["forbidden"]["code"]
+            return jsonify(error["forbidden"]), error["forbidden"]["code"]
 
         if "lastname" in request_json["data"] and request_json["data"]["lastname"]:
             insert_with_lastname_query = sqlalchemy.text("INSERT INTO users (username, email, "
@@ -123,10 +121,10 @@ def user_access(request):
                 db.execute(insert_with_lastname_query,
                            request_json["data"])
                 db.commit()
-                # return "inserted with lastname"
+
             except SQLAlchemyError as e:
                 error = str(e.__dict__['orig'])
-                return error, 500
+                return jsonify({"status": "fail", "error": error}), 500
 
         else:
             insert_without_lastname_query = sqlalchemy.text("INSERT INTO users (username, email, firstname, password)"
@@ -138,21 +136,21 @@ def user_access(request):
 
             except SQLAlchemyError as e:
                 error = str(e.__dict__['orig'])
-                return error, 500
+                return jsonify({"status": "fail", "error": error}), 500
 
         try:
             user = db.execute(get_user_query, request_json["data"]).fetchone()
 
         except SQLAlchemyError as e:
             error = str(e.__dict__['orig'])
-            return error, 500
+            return jsonify({"status": "fail", "error": error}), 500
 
         if user:
             user = {"id": user[0], "username": user[1], "firstname": user[2],
                     "lastname": user[3], "email": user[4], "date_time": user[5]}
-            return jsonify(user), 201
+            return jsonify({"status": "pass", "user": user}), 201
         else:
-            return error["unauthorised"]["message"], error["unauthorised"]["code"]
+            return jsonify(error["unauthorised"]), error["unauthorised"]["code"]
 
     else:
         return "you can only register or login"
--- functions/test.py
+++ functions/test.py
@@ -1,13 +1,12 @@
 import requests
 import json
-dictToSend = {"data": {"email": "aaa@gmial.com",
+dictToSend = {"data": {"email": "agmial.com",
                        "password": "*71237**123712383",
                        "username": "123",
                        "firstname": "123",
                        "lastname": "123"},
               "request": "login"}
-#res = requests.post('https://us-central1-cloudcomputinglab-291822.cloudfunctions.net/user_access', json=json.dumps(dictToSend))
-print(isinstance(json.dumps(dictToSend), str))
-res = requests.post('http://127.0.0.1:8080/', json=json.dumps(dictToSend))
+res = requests.post('https://us-central1-cloudcomputinglab-291822.cloudfunctions.net/user_access', json=dictToSend)
+# res = requests.post('http://127.0.0.1:8080/', json=dictToSend)
 
 print(res.text, res.status_code)
